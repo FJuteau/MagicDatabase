cfba10a6483cf3867a0d24295af57dd2d940c373
François Juteau <fjuteau@vente-privee.com>
2019-03-24 21:43:00 +0100

    Project creation

---
+++ MagicDatabase/MagicDatabase/Sources/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MagicDatabase
+//
+//  Created by François Juteau on 24/03/2019.
+//  Copyright © 2019 Momo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
