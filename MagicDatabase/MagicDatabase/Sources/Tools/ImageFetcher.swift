5ced9b540150e620aa1cef0dd9c2c53eb8b91b7d
François Juteau <fjuteau@vente-privee.com>
2019-03-26 10:53:36 +0100

    base creation

---
+++ MagicDatabase/MagicDatabase/Sources/Tools/ImageFetcher.swift
@@ -0,0 +1,21 @@
+//
+//  ImageFetcher.swift
+//  MagicDatabase
+//
+//  Created by François Juteau on 26/03/2019.
+//  Copyright © 2019 Momo. All rights reserved.
+//
+
+import UIKit
+
+final class ImageFetcher {
+
+    /// Fetch Image from network
+    ///
+    /// - Parameters:
+    ///   - url: url fron where to get the image
+    ///   - completionHandler: returns a placeholder image while loading, then returns the fetched image
+    func getImage(from url: URL, completionHandler: ((UIImage) -> Void)) {
+
+    }
+}
